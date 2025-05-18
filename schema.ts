29b294e7ecb0b0f4f39c25289ff99b868aa098a3
Ben <benjamin.scottt.dev@gmail.com>
2025-05-18 12:03:22 -0600

    feat: add chain models
    - chains
    - chain steps

--- schema.ts
+++ schema.ts
@@ -1,9 +1,43 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp, integer } from 'drizzle-orm/pg-core';
 
 export const subscribersTable = pgTable('subscribers', {
   id: serial('id').primaryKey(),
   email: text('email').notNull().unique(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
+});
+
+export const usersTable = pgTable('users', {
+  id: serial('id').primaryKey(),
+  email: text('email').notNull().unique(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').notNull().$onUpdateFn(() => new Date())
+});
+
+export const chainsTable = pgTable('chains', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id').notNull().references(() => usersTable.id, { onDelete: 'cascade' }),
+
+  name: text('name'),
+
+  cycleCount: integer('cycle_count').notNull(),
+  currentCycle: integer('current_cycle').notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').notNull().$onUpdateFn(() => new Date()),
+});
+
+export const chainStepsTable = pgTable('chain_steps', {
+  id: serial('id').primaryKey(),
+  chainId: integer('chain_id').notNull().references(() => chainsTable.id, { onDelete: 'cascade' }),
+
+  prompt: text('prompt').notNull(),
+  result: text('result'),
+
+  cycleCount: integer('cycle_count').notNull(),
+  currentCycle: integer('current_cycle').notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').notNull().$onUpdateFn(() => new Date())
 });
 
 export type InsertSubscriber = typeof subscribersTable.$inferInsert;
