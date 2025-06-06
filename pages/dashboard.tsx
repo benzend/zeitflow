91010a2df15048cc12c9b0f2c5fc3343fbb6f488
Ben <benjamin.scottt.dev@gmail.com>
2025-06-06 07:49:37 -0600

    chore(dashboard): add more vertical space

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -214,8 +214,8 @@ export default function Dashboard() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto px-4 py-10 mt-10 max-w-6xl min-h-[90vh]">
-        <nav className="mb-20 flex justify-between items-center">
+      <main className="container mx-auto px-4 py-10 max-w-6xl min-h-[90vh]">
+        <nav className="mb-10 flex justify-between items-center">
           <ul className="flex gap-4">
             <li>
               <Link href="/dashboard">
@@ -243,7 +243,7 @@ export default function Dashboard() {
         )}
 
         <div className="flex gap-4">
-          <section className="w-1/3 bg-foreground rounded-lg h-[calc(100vh-250px)] relative">
+          <section className="w-1/3 bg-foreground rounded-lg h-[calc(100vh-180px)] relative">
             {/* Tab Header */}
             <div className="-mt-4 flex justify-end">
               <div className="px-4 py-2 bg-foreground rounded-lg">
@@ -303,7 +303,7 @@ export default function Dashboard() {
             </div>
           </section>
 
-          <section className="w-2/3 bg-foreground rounded-lg h-[calc(100vh-250px)]">
+          <section className="w-2/3 bg-foreground rounded-lg h-[calc(100vh-180px)]">
             <div className="-mt-4 flex justify-end">
               <div className="px-4 py-2 bg-foreground rounded-lg">
                 <h2 className="text-sm font-bold text-center text-primary">
