623c631d193a84545d4d9b2a6520508b81dc0e1d
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2025-10-01 22:26:00 -0600

    chore: don't need env config, next does this automatically

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { config } from 'dotenv';
-
-config({ path: '.env.local' });
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL missing');
 
