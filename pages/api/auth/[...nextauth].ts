027d953411c8a6ccd4234e299cc6b3514de5767a
Ben <benjamin.scottt.dev@gmail.com>
2026-01-17 23:16:37 -0700

    fix: google oauth signin

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -20,11 +20,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope: 'openid email profile https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events'
-        }
-      }
     }),
     CredentialsProvider({
       name: 'credentials',
